b2794945f184e0ee95c7d0f3da3841a964d94df0
Joutos <joaodemeto123@gmail.com>
2024-09-17 22:25:51 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Shapefile to KML Converter
+
+Este projeto é um conversor de arquivos Shapefile (.shp) para KML (.kml) usando Python. O código lê um arquivo Shapefile e converte suas geometrias em um formato KML, que pode ser utilizado em aplicações como Google Earth.
+
+## Funcionalidades
+
+- Lê arquivos Shapefile, incluindo os arquivos .shp, .shx, e .dbf.
+- Converte geometrias de Shapefile em KML.
+- Gera um arquivo KML que pode ser utilizado em software de visualização de mapas.
+
+## Requisitos
+
+Antes de executar o código, você precisa ter o Python 3 instalado. Além disso, você deve instalar as bibliotecas necessárias. As bibliotecas utilizadas são:
+
+- `lxml`: Para manipulação de XML.
+- `pykml`: Para criação e manipulação de arquivos KML.
+- `pyshp`: Para leitura de arquivos Shapefile.
+
+## Uso 
+
+``python shp2kml.py <caminho_para_o_arquivo.shp> [<caminho_para_o_arquivo.prj>]
+``
---
+++ shpToKml.py
@@ -0,0 +1,70 @@
+from lxml import etree
+from pykml.factory import KML_ElementMaker as KML
+import shapefile
+import sys
+import os
+
+
+def create_reader(shp_fname):
+    return shapefile.Reader(shp_fname)
+
+
+def read_projection(prj_fname):
+    with open(prj_fname, 'r') as prj_file:
+        return prj_file.read()
+
+
+def create_kml_root():
+    doc = KML.kml()
+    document = KML.Document()   
+    doc.append(document)
+    return document
+
+
+def main(args):
+    if len(args) < 2:
+        print("Usage: script.py <shapefile> [<projection file>]")
+        return 1
+
+    shp_fname = args[1]
+    prj_fname = args[2] if len(args) > 2 else None
+
+    if not os.path.isfile(shp_fname):
+        print(f"Shapefile {shp_fname} not found.")
+        return 1
+
+    base_name, _ = os.path.splitext(shp_fname)
+    if not (os.path.isfile(f"{base_name}.shx") and os.path.isfile(f"{base_name}.dbf")):
+        print(f"Related files for {shp_fname} are missing.")
+        return 1
+
+    reader = create_reader(shp_fname)
+
+    if prj_fname and os.path.isfile(prj_fname):
+        projection = read_projection(prj_fname)
+        print("Projection info:", projection)
+
+    document = create_kml_root()
+
+    for shapeRec in reader.shapeRecords():
+        name = shapeRec.record[1]
+        points = shapeRec.shape.points
+
+        coordinates = ','.join([f'{p[0]},{p[1]}' for p in points])
+
+        placemark = KML.Placemark(
+            KML.name(name),
+            KML.LineString(
+                KML.coordinates(coordinates)
+            )
+        )
+        document.append(placemark)
+
+    with open('saida.kml', 'wb') as f:
+        f.write(etree.tostring(KML.kml(document), pretty_print=True))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
